b5bbd7d58c87db61209d3cc8ab83c8f3f9c47dc5
Emerson Schafhauser <emerson@unc.br>
2023-08-14 20:24:55 -0300

    Criação do model Book

---
+++ models/book.js
@@ -0,0 +1,17 @@
+// definindo a classe 'Book'
+class Book {
+  // implementando o 'construtor' da classe
+  constructor (
+    id,
+    titulo,
+    tituloOriginal,
+    genero,
+    anoLancamento
+  ) {
+    this.id = id
+    this.titulo = titulo,
+    this.tituloOriginal = tituloOriginal,
+    this.genero = genero,
+    this.anoLancamento = anoLancamento
+  }
+}
