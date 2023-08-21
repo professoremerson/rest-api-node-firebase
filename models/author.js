bc4b01b1139a2548dc87c872c0210dce20bd7e20
Emerson Schafhauser <emerson@unc.br>
2023-08-21 19:30:03 -0300

    Criação dos models para Autor e Editora

---
+++ models/author.js
@@ -0,0 +1,19 @@
+// definindo a classe 'Author'
+class Author {
+  // implementando o 'construtor' da classe
+  constructor (
+    id,
+	  nome,
+	  genero,
+	  nacionalidade,
+	  nascimento
+  ) {
+    this.id = id
+    this.nome = nome,
+    this.genero = genero,
+    this.nacionalidade = nacionalidade,
+    this.nascimento = nascimento
+  }
+}
+
+module.exports = Author
---
+++ models/publisher.js
@@ -0,0 +1,19 @@
+// definindo a classe 'Publisher'
+class Publisher {
+  // implementando o 'construtor' da classe
+  constructor (
+    id,
+    nome,
+    pais,
+    site,
+    contato
+  ) {
+    this.id = id
+    this.nome = nome,
+    this.pais = pais,
+    this.site = site,
+    this.contato = contato
+  }
+}
+
+module.exports = Publisher
